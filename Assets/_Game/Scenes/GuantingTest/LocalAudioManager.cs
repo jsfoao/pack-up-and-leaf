764e913f9a6ca58ced1f9ac36decee1177ad325d
fragileMind <90679956+fragileMind@users.noreply.github.com>
2022-05-05 00:06:27 +0200

    Vase/Bee Volume control
    now the volumes are correctly control by the option menu.

--- Assets/_Game/Scenes/GuantingTest/LocalAudioManager.cs
+++ Assets/_Game/Scenes/GuantingTest/LocalAudioManager.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class LocalAudioManager : MonoBehaviour
 {
     public Hashtable localAudioHash;
     public GameObject[] localAudioObjects;
     public  float volumeAtPoint = 0.5f;
+
+    [SerializeField] AudioMixer audioMixer;
+    private float masterValueFloat;
+    private float volumeValueFloat;
 
     private void Start()
     {
@@ -28,6 +33,26 @@ public class LocalAudioManager : MonoBehaviour
     {
         GameObject thisAudioObject = (GameObject)localAudioHash[name];
         AudioSource thisAudioSource = thisAudioObject.GetComponent<AudioSource>();
-        AudioSource.PlayClipAtPoint(thisAudioSource.clip, transform.position,volumeAtPoint);
+        bool masterValueResult = audioMixer.GetFloat("masterVolume", out float masterVolume);
+        if (masterValueResult)
+        {
+           masterValueFloat = (masterVolume+80)*5/4;
+        }
+        else
+        {
+            masterValueFloat = 0.0f;
+        }
+        bool volumValueResult = audioMixer.GetFloat("soundVolume", out float soundValue);
+        if (volumValueResult)
+        {
+            volumeValueFloat = (soundValue + 80) * 5 / 4;
+        }
+        else
+        {
+            volumeValueFloat = 0.0f;
+        }
+        float finalVolumeProduct = volumeAtPoint * masterValueFloat * volumeValueFloat;
+        Debug.Log("finalVolume"+finalVolumeProduct);
+        AudioSource.PlayClipAtPoint(thisAudioSource.clip, transform.position,finalVolumeProduct);
     }
 }
